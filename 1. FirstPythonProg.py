510111071ee03665fac1eec9f4885c0fabeeb0f6
Abhijeet chauhan <chauhanabhijeetofficial@gmail.com>
2023-10-15 01:56:58 +0530

    Second Commit

---
+++ 1. FirstPythonProg.py
@@ -0,0 +1,9 @@
+print("Hi Chauhan Sahab")
+
+print(8787*983)
+
+print('''
+Twinkle Twinkle little star,
+      how are wonder what you are
+
+''')
---
+++ Loop.py
@@ -0,0 +1,17 @@
+#THIS IS THE CODE FOR LOOP CONCEPT 
+#THIS IS THE CODE FOR LOOP CONCEPT 
+# i=0
+# while(True):
+#     print("Hi chauhan Sahab",i)
+#     i+=1
+
+#NESED WHILE LOOP
+i=1
+while (i<=5): 
+    print("Hi Chauhan",end="")
+    j=1
+    while(j<=4):
+        print(" Sahab ",end="")
+        j+=1    
+    i=i+1
+    print()
