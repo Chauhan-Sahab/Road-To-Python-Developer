04701fa39ac43cf9e000dc9bb6a9a26676fd322f
Abhijeet chauhan <chauhanabhijeetofficial@gmail.com>
2023-10-22 08:19:40 +0530

    Jadu

---
+++ Array.py
@@ -0,0 +1,14 @@
+from numpy import *
+#from array import *
+arr=[[2.0,2,2,2,2],[2,2,2,2,2]]
+for i in arr:
+    for j in i:
+        print(j,end="")
+    print()
+
+'''
+This is linspace in this we provide 
+'''
+arr1= linspace(0,15,17)
+print(arr1)
+#print("Hi Chauhan Sahab")
