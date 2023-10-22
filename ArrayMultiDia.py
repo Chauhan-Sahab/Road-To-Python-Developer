b21b3e6e01f6d049cdd801cfca388dbe131e2670
Abhijeet chauhan <chauhanabhijeetofficial@gmail.com>
2023-10-22 08:19:56 +0530

    jadu1

---
+++ ArrayMultiDia.py
@@ -0,0 +1,4 @@
+from numpy import *
+
+
+arr= array([[2,2],[3,3]])
---
+++ manishExplain.py
@@ -0,0 +1,10 @@
+def Dynamic(id,*name):
+    print(id)
+    for i in name:
+        print(i)
+    # for i,j in name.items():
+    #         print(i,j)
+
+
+
+Dynamic(1,"Chauhan","Sahab","Manish")
